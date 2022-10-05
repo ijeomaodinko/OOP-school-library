396e486c8b0de12f9ad45e38c4d5f8a1005c0d91
ijeoma <ijeomaodinko>
2022-10-05 10:00:41 -0400

    Add codes for teacher's class

--- person.rb
+++ person.rb
@@ -1,51 +1,32 @@
 class Person 
-    #accessors
+    #the getters and setters accessor
     attr_reader :id
     attr_accessor :name, :parent_permission
 
     #constructor to initialize the class with instance varibles
-    def initialize (name = "unknown", age, parent_permission = True)
+    def initialize (name = "unknown", age, parent_permission==True)
         @id = Random.rand (1..1000)
         @name = name
+        @age =age
         @parent_permission = true
     end 
 
-    # getters for @id, @name and @parent_permission
-
-    def id
-        @id
-    end
-
-    def name 
-        @name
-    end
-
-    def parent_permission
-        @parent_permission
-    end 
-
-    #setters for @name and @parent_permission
-
-    def name = (name)
-        @name = name
-    end
-
-    def parent_permission = (parent_permission)
-        @parent_permission = True
-    end
-
-    def is_of_age? (age)
-        if age >= 18
-           puts true
-           else puts false 
-        end
-    end 
-
-    def can_use_services? (age, parent_permission)
-        if age >= 18 && parent_permission = true
+    def can_use_services? 
+        if age >= 18 && parent_permission
             return true
         end
     end 
+
+    #private method
+    private 
+
+        def is_of_age? (age)
+            
+            if age >= 18
+               puts true
+               else puts false 
+            end
+        end 
 
 
 end

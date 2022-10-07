bbb196ab0815fa939ea2bb2ee79045529dd3f131
ijeoma <ijeomaodinko>
2022-10-07 18:17:23 -0400

    Fix linters errors

--- main.rb
+++ main.rb
@@ -1,13 +1,13 @@
 require './app'
 
 def main
-    app = App.new
-    puts "Welcome to OOP School Library App!"
-    option_choice(app)
+  app = App.new
+  puts 'Welcome to OOP School Library App!'
+  option_choice(app)
 end
 
 def list_options
-    puts "Please choose an option by enteringa number:
+  puts "Please choose an option by enteringa number:
     1. List all books
     2. List all people
     3. Create a person
@@ -16,35 +16,40 @@ def list_options
     6. List all rentals for a given person id
     7. Exit
     "
-    
-    def option_choice(app)
-        option = input_option
-    case choice
-    when 1
-        app.list_books
-    when 2
-        app.list_people
-    when 3
-        app.create_person
-    when 4
-        app.create_book
-    when 5
-        app.create_rental
-    when 6
-        app.list_rentals_by_id
-    else 
-        puts "Good Bye 👋"
-    end 
-end 
-
-def user_choice 
-    input_option = gets.chomp.to_i
-    while input_option > 7 || input_option < 1
-        puts "Please enter a valid option between 1 to 7"
-        input_option = gets.chomp.to_i
-    end 
-    input_option
 end
-    
+
+def option_choice(app)
+  loop do
+    list_options
+
+    option = user_choice
+
+    case option
+    when 1
+      app.list_books
+    when 2
+      app.list_people
+    when 3
+      app.create_person
+    when 4
+      app.create_book
+    when 5
+      app.create_rental
+    when 6
+      app.list_rentals_by_id
+    else
+      puts 'Good Bye 👋'
+    end
+  end
+end
+
+def user_choice
+  option = gets.chomp.to_i
+  while option > 7 || option < 1
+    puts 'Please enter a valid option between 1 to 7'
+    option = gets.chomp.to_i
+  end
+  option
+end
 
 main
--- person.rb
+++ person.rb
@@ -38,9 +38,9 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-capitalized_trimmed_person.correct_name
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# capitalized_trimmed_person.correct_name
